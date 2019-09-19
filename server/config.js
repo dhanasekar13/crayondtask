f9ea584a75a81051b4c345225a0d43d651804474
dhanasekar13 <mail2dhanasekar13@gmail.com>
2019-09-19 09:18:00 -0700

    --Project setup--

---
+++ server/config.js
@@ -0,0 +1,11 @@
+const local = {
+    url:'http://localhost:1111',
+    description: 'local configuration details',
+}
+const production = {
+    url :'http://anydomain.com',
+    description: 'production configuration details'
+}
+
+export default local
+module.export = local
